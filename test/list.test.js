8f804b6e9f183da0df82e6f6b7edd7523d5476e3
Tj Holowaychuk <tj@vision-media.ca>
2011-05-01 17:27:57 -0700

    more tests

--- test/list.test.js
+++ test/list.test.js
@@ -21,7 +21,24 @@ module.exports = {
     pets.length(function(err, len){
       should.equal(null, err);
       len.should.equal(3);
-      done();
+      pets.rpop(function(err, pet){
+        pet.should.equal('jane');
+        done();
+      });
+    });
+  },
+  
+  '.lpush(str)': function(done){
+    pets.lpush('tobi');
+    pets.lpush('loki');
+    pets.lpush('jane');
+    pets.length(function(err, len){
+      should.equal(null, err);
+      len.should.equal(3);
+      pets.lpop(function(err, pet){
+        pet.should.equal('jane');
+        done();
+      });
     });
   },
 
