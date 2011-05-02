a007ab2cfcd07f952860ae90ebfbe309dc6854dc
Tj Holowaychuk <tj@vision-media.ca>
2011-05-02 11:31:53 -0700

    basic social graph example

---
+++ examples/social-graph.js
@@ -0,0 +1,82 @@
+
+/**
+ * Module dependencies.
+ */
+
+var kit = require('../')
+  , redis = require('redis')
+  , client = redis.createClient();
+
+function User(name) {
+  this.name = name;
+}
+
+User.find = function(ids, fn){
+  var pending = ids.length
+    , users = [];
+  ids.forEach(function(id){
+    client.get('user:' + id + ':name', function(err, name){
+      var user = new User(name);
+      user.id = id;
+      users.push(user);
+      --pending || fn(null, users);
+    });
+  });
+};
+
+User.prototype.follow = function(user, fn){
+  fn = fn || function(){};
+  client.sadd('user:' + this.id + ':follows', user.id);
+  client.sadd('user:' + user.id + ':followers', this.id, fn);
+};
+
+User.prototype.followers = function(fn){
+  client.smembers('user:' + this.id + ':followers', function(err, ids){
+    if (err) return fn(err);
+    User.find(ids, fn);
+  });
+};
+
+User.prototype.follows = function(fn){
+  client.smembers('user:' + this.id + ':follows', function(err, ids){
+    if (err) return fn(err);
+    User.find(ids, fn);
+  });
+};
+
+User.prototype.friends = function(fn){
+  client.sinter(
+      'user:' + this.id + ':followers'
+    , 'user:' + this.id + ':follows'
+    , function(err, ids){
+      if (err) return fn(err);
+      User.find(ids, fn);
+    });
+};
+
+User.prototype.save = function(fn){
+  var self = this;
+  fn = fn || function(){};
+  client.incr('users', function(err, id){
+    if (err) return fn(err);
+    self.id = id;
+    client.set('user:' + id + ':name', self.name);
+    fn(null, id);
+  });
+};
+
+var tobi = new User('tobi')
+  , loki = new User('loki')
+  , jane = new User('jane');
+
+tobi.save();
+loki.save();
+jane.save(function(){
+  tobi.follow(loki);
+  loki.follow(tobi);
+  loki.follow(jane);
+  jane.follow(tobi);
+  tobi.friends(function(err, users){
+    console.log(users);
+  });
+});
